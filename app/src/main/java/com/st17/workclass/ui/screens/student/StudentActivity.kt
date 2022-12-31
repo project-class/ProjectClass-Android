812c604a9a6f99f8eb8fca05d33056b6dcc41c47
Alexey <91087810+AI-Kusu@users.noreply.github.com>
2022-12-31 15:29:06 +0500

    create new activity

--- app/src/main/java/com/st17/workclass/ui/screens/student/StudentActivity.kt
+++ app/src/main/java/com/st17/workclass/ui/screens/student/StudentActivity.kt
@@ -1,9 +1,10 @@
-package com.st17.workclass
+package com.st17.workclass.ui.screens.student
 
-import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -14,11 +15,12 @@ import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.st17.workclass.ui.background.mainBackground
 import com.st17.workclass.ui.navigation.rootGraph
-import com.st17.workclass.ui.screens.student.StudentActivity
+import com.st17.workclass.ui.navigation.studentGraph
 import com.st17.workclass.ui.theme.WorkClassTheme
 
 
-class MainActivity : ComponentActivity() {
+class StudentActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -30,12 +32,10 @@ class MainActivity : ComponentActivity() {
                     Color.Transparent, darkIcons = MaterialTheme.colors.isLight)
 
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    mainBackground()
+                    studentGraph()
                 }
             }
         }
-
-        this.startActivity(Intent(this, StudentActivity::class.java))
 
         val settings = getSharedPreferences("PreferencesName", MODE_PRIVATE)
     }
